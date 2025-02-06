5e77c59a3123cb662f7065e04987b672b9c0db3c
William-Dev <williamlegall5@gmail.com>
2025-02-06 16:33:01 +0100

    Header ok !

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,20 +1,20 @@
-import { useNavigate } from 'react-router-dom';
-import './Header.css'
-import CroMagnon from "../assets/CroMagnon.webp"
+import { useNavigate } from "react-router-dom";
+
+import "./Header.css";
+import CroMagnon from "../assets/CroMagnon.webp";
 
 function Header() {
-  
   return (
     <header>
       <div className="full-header">
         <h2>Bienvenue au Cromagnon Lodge!</h2>
-      <div className='logo-header'>
-        <img src={CroMagnon}/>
+        <div className="logo-header">
+          <img src={CroMagnon} alt="Presentation Entreprise" />
+        </div>
+        <h2>Trouvez votre Refuge</h2>
       </div>
-      <h2>Trouvez votre Refuge</h2>
-      </div>
+      <div className="hero">{/* backgrund-image trop styler */}</div>
     </header>
-    
   );
 }
 
